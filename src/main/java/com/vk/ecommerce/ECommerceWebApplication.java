f349d8231f95076cd3d32c3694bab8954c48d96b
Vinay Kumar <vk.mca.infotech@gmail.com>
2025-01-01 13:53:00 +0530

    ECommerce Project Initial Draft

---
+++ src/main/java/com/vk/ecommerce/ECommerceWebApplication.java
@@ -0,0 +1,13 @@
+package com.vk.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceWebApplication.class, args);
+	}
+
+}
