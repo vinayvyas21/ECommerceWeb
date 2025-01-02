ffbcbef39a9fa23b2f2d558e0d3f4d1bbdfd680a
Vinay Kumar <vk.mca.infotech@gmail.com>
2025-01-02 17:06:45 +0530

    Exception Handling

---
+++ src/main/java/com/vk/ecommerce/advices/ExceptionsControllerAdvice.java
@@ -0,0 +1,20 @@
+package com.vk.ecommerce.advices;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.vk.ecommerce.dtos.ExceptionDTO;
+
+@RestControllerAdvice
+public class ExceptionsControllerAdvice {
+	
+	@ExceptionHandler(ArithmeticException.class)
+	public ResponseEntity<ExceptionDTO> handleArithmaticException(ArithmeticException ex) {
+		ExceptionDTO dto = new ExceptionDTO();
+		dto.setMessage(ex.getMessage());
+		return new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
+	}
+
+}
---
+++ src/main/java/com/vk/ecommerce/dtos/ExceptionDTO.java
@@ -0,0 +1,11 @@
+package com.vk.ecommerce.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExceptionDTO {
+
+	private String message;
+}
