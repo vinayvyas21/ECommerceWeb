fd80d0704ae356b76696ee541303700f039c5541
Vinay Kumar <vk.mca.infotech@gmail.com>
2025-03-27 11:53:29 +0530

    Establish Database Connection and understanding of cardanalities

---
+++ src/main/java/com/vk/ecommerce/models/BaseModel.java
@@ -0,0 +1,20 @@
+package com.vk.ecommerce.models;
+
+import java.util.Date;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseModel {
+	@Id
+	private Long id;
+	
+	private Date createdAt;
+	private Date updatedAt;
+
+}
